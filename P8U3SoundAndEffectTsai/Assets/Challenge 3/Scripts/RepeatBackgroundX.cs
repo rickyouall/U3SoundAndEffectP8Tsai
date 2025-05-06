7230068c6621c38f81927041aa5e72c48259a490
rickyouall <740502@student.dusd.net>
2025-05-06 15:22:21 -0700

    making the whole game
    Today I finished up every bug i find so far and added some sound. I also started Challange 3.

---
+++ P8U3SoundAndEffectTsai/Assets/Challenge 3/Scripts/RepeatBackgroundX.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackgroundX : MonoBehaviour
+{
+ 
+    private Vector3 startPos;
+    public float repeat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.x < startPos.x - repeat)
+        {
+            transform.position = startPos;
+        }
+    }
+}
+
+ 
+
+
+
